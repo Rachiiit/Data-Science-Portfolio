76faa0d229e6a479a38a486126ef1a6b204f7d55
Rachiiit <90087769+Rachiiit@users.noreply.github.com>
2021-10-26 09:14:13 +0530

    Python files

---
+++ Sir practice files/module1_class14.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    #a = 8
+    #b = 4
+    c = a+b
+    print(c)
+    return c
+
+def subtract():
+    a = 8
+    b = 4
+    print(a-b)
+    return
+
+mo1 = "Hello"
+
+d = {1:2}
---
+++ Sir practice files/module2_class14.py
@@ -0,0 +1,18 @@
+def add():
+    a = 8
+    b = 4
+    c = 2
+    print(a+b+c)
+    return
+
+def subtract():
+    a = 8
+    b = 4
+    c = 2
+    print(a-b-c)
+    return
+
+mo2 = "Hi"
+
+
+
